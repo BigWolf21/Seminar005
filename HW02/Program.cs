95a13e149cc03e0b50a2a10d123a77a304317d97
BigWolf21 <122544707+BigWolf21@users.noreply.github.com>
2023-02-26 13:28:11 +0700

    Создание HW02 решение 2-ой задачи, через методы

---
+++ HW02/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int GetNumber(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreateArray(int size)
+{
+  return new int[size];
+}
+
+void FillArrayRandom(int[] collection)
+{
+  int size = collection.Length;
+  int index = 0;
+  while (index < size)
+  {
+    collection[index] = new Random().Next(-100, 100);
+    index++;
+  }
+
+}
+
+string Print(int[] array)
+{
+  string result = String.Empty;
+  int len = array.Length;
+  int index = 0;
+  while (index < len)
+  {
+    result = result + " " + array[index];
+    index++;
+  }
+  return result;
+}
+
+int size = GetNumber("Введите размер массива - ");
+int[] value = CreateArray(size);
+FillArrayRandom(value);
+Console.WriteLine(Print(value));
+
+int summa = 0;
+int index = 0;
+while (index < size)
+{
+  if (index % 2 == 1)
+  {
+    summa += value[index];
+    index++;
+  }
+  else
+  {
+    index++;
+  }
+}
+
+Console.WriteLine("Сумма элементов, стоящих на нечётных позициях в массиве = " + summa);
